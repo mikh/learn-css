95b0fd0190fd67e8ea3bf125faa087d21f2493eb
Mikhail Andreev <mandreev@abinitio.com>
2024-09-05 14:36:02 -0400

    Add Basic

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,36 @@
+import { useState } from "react";
+
 import "./App.css";
+import Basic from "./Basic";
 
 function App() {
-  return <></>;
+  const [cssConfig, setCssConfig] = useState(null);
+
+  function handleCssConfigChange(e) {
+    switch (e.target.value) {
+      case "":
+        setCssConfig(null);
+        break;
+      case "basic":
+        setCssConfig(<Basic></Basic>);
+        break;
+      default:
+        console.log(`Unknown value ${e.target.value}`);
+    }
+  }
+
+  return (
+    <div className="flex flex-col items-center">
+      <h1 className="text-center">Select CSS Config</h1>
+      <div>
+        <select onChange={handleCssConfigChange}>
+          <option value=""></option>
+          <option value="basic">Basic</option>
+        </select>
+        {cssConfig}
+      </div>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Basic.jsx
@@ -0,0 +1,7 @@
+export default function Basic() {
+  return (
+    <div className="w-52 h-44 border-solid border rounded-md border-black bg-slate-400 flex justify-center items-center text-3xl">
+      1
+    </div>
+  );
+}
